432026d55ec674ba224c7d6076b8019d7e2edc23
Yashwanth Anand S <yashwanthanands@outlook.com>
2023-06-09 17:42:42 +0530

    Committed bean scopes concept implementation

---
+++ 07-bean-scopes/src/main/java/com/yash/dev/springcoredemo/utils/CricketCoach.java
@@ -0,0 +1,19 @@
+package com.yash.dev.springcoredemo.utils;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author yashwanthanands
+ */
+@Component
+public class CricketCoach implements Coach{
+
+    public CricketCoach(){
+        System.out.println("In constructor : "+getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes";
+    }
+}
